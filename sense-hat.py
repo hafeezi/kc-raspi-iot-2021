e9e7ff7b0ffa16e94aa706c2c6d9b31055af947d
Ahmad Hafeezi Ali Amat <hafeezi.ahmad@gmail.com>
2021-06-18 23:15:46 +0800

    UPDATE: Initial commit

---
+++ sense-hat.py
@@ -0,0 +1,20 @@
+# from sense_hat import SenseHat
+import requests
+
+url = "https://athena.sabah.io/weather/kota-kinabalu.json"
+
+r = requests.get(url = url)
+
+data = r.json()
+
+temp = data["current"]["temp"]
+feel_like = data["current"]["feels_like"]
+hum = data["current"]["humidity"]
+
+print(temp)
+print(feel_like)
+print(hum)
+
+# sense = SenseHat()
+
+# sense.show_message(temp + "C")
